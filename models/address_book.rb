90181319796841de3dfc6e31f0b508394fec16d1
Thal <tsyhabout@gmail.com>
2018-04-27 01:24:32 -0700

    complete reading CSV file

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 
 class AddressBook
     attr_reader :entries
@@ -28,4 +29,20 @@ class AddressBook
       end
       entries.delete(entry_remove)
     end
+    
+    def import_from_csv(file_name)
+        #implmentation goes here
+        csv_text = File.read(file_name)
+        csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+        # #8
+        csv.each do |row|
+            row_hash = row.to_hash
+            add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+        end
+    end
 end
+
+
+
+
+
